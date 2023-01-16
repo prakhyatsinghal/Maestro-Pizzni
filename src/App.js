06ae667b5c08d9e6311a3ed72478154b1e1cf8f2
Prakhyat Singhal <singhalprakhyat2001@gmail.com>
2023-01-16 22:24:38 +0530

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Header from './components/Header'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div>  
-      <Header></Header>  
+      <Header></Header>
+      <Navbar></Navbar>  
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className="banner">Header</div>
+    <div className="banner">
+        <div className = "banner_content">
+            <div className = "container">
+                <div className="banner__text">
+                <h3>Pizza Delivery</h3>
+                <h1>MAESTRO PIZINNI</h1>
+                <p>Hello! Meastro Pizinni here. We deliver tasty and delicious pizzas at your doorstep. We are having
+                    multiple varities of pizzas. 
+                </p>
+            </div>
+            </div>
+        </div>
+    </div>
+
   )
 }
 
