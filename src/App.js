d31db7338955e5b08b5a94e8bbcd4b30b58b97ee
Prakhyat Singhal <singhalprakhyat2001@gmail.com>
2023-01-22 11:24:43 +0530

    Completed Clients

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from './components/About';
 import Shop from './components/Shop';
 import Menu from './components/Menu';
 import Clients from './components/Clients';
+import Prices from './components/Prices';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Shop></Shop>
       <Menu></Menu>
       <Clients></Clients>
+      <Prices></Prices>
     </div>
   );
 }
